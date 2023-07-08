8bca57fb3ba53b40da83b7ea70cc057e0a16f58a
lilmazy <chukwuemekamarvellous284@gmail.com>
2023-07-08 07:09:31 -0700

    my first code

---
+++ introduction.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main(){
+ int height_o , base_o , sum;
+ print("Input height");
+ scanf("%f" , &height_o);
+ printf("Input base");
+ scanf("%f" , &base_o);
+sum=height_o*base_o;
+printf("your sum is..."  , &sum);
+
+return 0;
+
+
+
+
+}
